a6911c4d61262600fe0657bc07da4fed011a43f2
zeteny12 <zetus0512@gmail.com>
2024-09-05 17:15:26 +0200

    adatbázis

---
+++ techshop.sql
@@ -0,0 +1,249 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Gép: 127.0.0.1
+-- Létrehozás ideje: 2024. Sze 05. 17:13
+-- Kiszolgáló verziója: 10.4.28-MariaDB
+-- PHP verzió: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Adatbázis: `techshop`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `kategoriak`
+--
+
+CREATE TABLE `kategoriak` (
+  `id` int(11) NOT NULL,
+  `kategoria` varchar(255) NOT NULL,
+  `letrehozva` timestamp NOT NULL DEFAULT current_timestamp(),
+  `modositva` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- A tábla adatainak kiíratása `kategoriak`
+--
+
+INSERT INTO `kategoriak` (`id`, `kategoria`, `letrehozva`, `modositva`) VALUES
+(1, 'Okos otthoni eszközök', '2024-06-20 11:00:56', '2024-06-20 11:00:56'),
+(2, 'Mobiltelefon kiegészítők', '2024-06-20 11:00:56', '2024-06-20 11:00:56'),
+(3, 'Perifériák', '2024-06-20 11:00:56', '2024-06-20 11:00:56'),
+(4, 'Hardverek', '2024-06-20 11:00:56', '2024-06-20 11:00:56'),
+(5, 'Hordozható eszközök', '2024-06-20 11:00:56', '2024-06-20 11:00:56');
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `rendeles`
+--
+
+CREATE TABLE `rendeles` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `fizetendo` decimal(10,2) NOT NULL,
+  `statusz` varchar(255) NOT NULL,
+  `letrehozva` timestamp NOT NULL DEFAULT current_timestamp(),
+  `modositva` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `rendelesi_termekek`
+--
+
+CREATE TABLE `rendelesi_termekek` (
+  `id` int(11) NOT NULL,
+  `rendeles_id` int(11) NOT NULL,
+  `termek_id` int(11) NOT NULL,
+  `darabszam` int(11) NOT NULL,
+  `ar` decimal(10,2) NOT NULL,
+  `letrehozva` timestamp NOT NULL DEFAULT current_timestamp(),
+  `modositva` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `szallitasi_cim`
+--
+
+CREATE TABLE `szallitasi_cim` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `varos` varchar(255) NOT NULL,
+  `irszam` varchar(10) NOT NULL,
+  `utca_hszam` varchar(255) NOT NULL,
+  `emelet_ajto` varchar(255) DEFAULT NULL,
+  `telszam` varchar(20) DEFAULT NULL,
+  `fiz_mod` varchar(11) DEFAULT NULL,
+  `letrehozva` timestamp NOT NULL DEFAULT current_timestamp(),
+  `modositva` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `termekek`
+--
+
+CREATE TABLE `termekek` (
+  `id` int(11) NOT NULL,
+  `termek_neve` varchar(255) NOT NULL,
+  `leiras` text DEFAULT NULL,
+  `ar` decimal(10,2) NOT NULL,
+  `raktaron` int(11) NOT NULL,
+  `kategoria_id` int(11) NOT NULL,
+  `letrehozva` timestamp NOT NULL DEFAULT current_timestamp(),
+  `modositva` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `vezeteknev` varchar(255) NOT NULL,
+  `keresztnev` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `jelszo` varchar(255) NOT NULL,
+  `letrehozva` timestamp NOT NULL DEFAULT current_timestamp(),
+  `modositva` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexek a kiírt táblákhoz
+--
+
+--
+-- A tábla indexei `kategoriak`
+--
+ALTER TABLE `kategoriak`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- A tábla indexei `rendeles`
+--
+ALTER TABLE `rendeles`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- A tábla indexei `rendelesi_termekek`
+--
+ALTER TABLE `rendelesi_termekek`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `rendeles_id` (`rendeles_id`),
+  ADD KEY `termek_id` (`termek_id`);
+
+--
+-- A tábla indexei `szallitasi_cim`
+--
+ALTER TABLE `szallitasi_cim`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- A tábla indexei `termekek`
+--
+ALTER TABLE `termekek`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `kategoria_id` (`kategoria_id`);
+
+--
+-- A tábla indexei `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- A kiírt táblák AUTO_INCREMENT értéke
+--
+
+--
+-- AUTO_INCREMENT a táblához `kategoriak`
+--
+ALTER TABLE `kategoriak`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT a táblához `rendeles`
+--
+ALTER TABLE `rendeles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `rendelesi_termekek`
+--
+ALTER TABLE `rendelesi_termekek`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `szallitasi_cim`
+--
+ALTER TABLE `szallitasi_cim`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `termekek`
+--
+ALTER TABLE `termekek`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Megkötések a kiírt táblákhoz
+--
+
+--
+-- Megkötések a táblához `rendeles`
+--
+ALTER TABLE `rendeles`
+  ADD CONSTRAINT `rendeles_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Megkötések a táblához `rendelesi_termekek`
+--
+ALTER TABLE `rendelesi_termekek`
+  ADD CONSTRAINT `rendelesi_termekek_ibfk_1` FOREIGN KEY (`rendeles_id`) REFERENCES `rendeles` (`id`),
+  ADD CONSTRAINT `rendelesi_termekek_ibfk_2` FOREIGN KEY (`termek_id`) REFERENCES `termekek` (`id`);
+
+--
+-- Megkötések a táblához `szallitasi_cim`
+--
+ALTER TABLE `szallitasi_cim`
+  ADD CONSTRAINT `szallitasi_cim_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Megkötések a táblához `termekek`
+--
+ALTER TABLE `termekek`
+  ADD CONSTRAINT `termekek_ibfk_1` FOREIGN KEY (`kategoria_id`) REFERENCES `kategoriak` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
